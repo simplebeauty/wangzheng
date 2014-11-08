f9e2df78680198e91c75a42d5bbccc7af949a8c3
qwvazheng0518 <qwvazheng@163.com>
2014-11-08 11:24:46 +0800

    点击按钮跳转下一个Activity

--- HelloWorld/src/com/example/helloworld/DisplayMessageActivity.java
+++ HelloWorld/src/com/example/helloworld/DisplayMessageActivity.java
@@ -1,38 +1,36 @@
 package com.example.helloworld;
 
+import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-//import android.widget.TextView;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-	public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
-	
+public class DisplayMessageActivity extends ActionBarActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//TextView tv=new TextView(this);
-		//tv.setText("Hello,Android!");
-		//setContentView(tv);
-		setContentView(R.layout.activity_main);
-	}
-	
-	public void sendMessage(View view){
-		Intent intent=new Intent(this,DisplayMessageActivity.class);
-		EditText editText=(EditText)findViewById(R.id.edit_message);
-		String message=editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+		//setContentView(R.layout.activity_display_message);
+		
+		// Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+	    
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.display_message, menu);
 		return true;
 	}
 
